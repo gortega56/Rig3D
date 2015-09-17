7cdbfd41dde05d7f7ad04df57318cf40a45fd6a2
Gabriel Ortega <gabriel.ortega56@gmail.com>
2015-09-16 21:09:43 -0400

    Added macro for char to wchar

--- Rig3D/rig_defines.h
+++ Rig3D/rig_defines.h
@@ -1,3 +1,11 @@
 #pragma once
 #define RIG_SUCCESS		0
 #define RIG_ERROR		1
+
+#define CSTR2WSTR(in, out)					\
+{											\
+wchar_t wtext[256];							\
+size_t num = 0;								\
+mbstowcs_s(&num, wtext, in, strlen(in) + 1);\
+out = wtext;								\
+}
